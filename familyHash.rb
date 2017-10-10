8b7b9c12ee6333089c5a22c1eafc952460914d32
jordanberghofer <jordan.berghofer@gmail.com>
2017-10-10 15:31:21 -0500

    added the family hash

---
+++ familyHash.rb
@@ -0,0 +1,28 @@
+spacer = "******"
+puts spacer
+
+
+my_family = {
+    :mother => {
+        :name => 'Carole',
+        :age => 62
+    },
+    :father => {
+        :name => 'Stephen',
+        :age => 62
+    },
+    :sister => {
+        :name => 'Caitlin',
+        :age => 34
+    },
+    :brother => {
+        :name => 'Nathan',
+        :age => 32
+    },
+    :self => {
+        :name => 'Jordan',
+        :age => 28
+    }
+}
+
+puts "#{my_family}"
