9446f07a0c093fb7c9631e911cd1972e64c8c6a3
jordanberghofer <jordan.berghofer@gmail.com>
2017-10-10 16:00:26 -0500

    final cut. This one was not actually too hard me-thinks

--- familyHash.rb
+++ familyHash.rb
@@ -4,25 +4,41 @@ puts spacer
 
 my_family = {
     :mother => {
+        :rel => 'my mother',
         :name => 'Carole',
         :age => 62
     },
     :father => {
+        :rel => 'my father',
         :name => 'Stephen',
         :age => 62
     },
     :sister => {
+        :rel => 'my sister',
         :name => 'Caitlin',
         :age => 34
     },
     :brother => {
+        :rel => 'my brother',
         :name => 'Nathan',
         :age => 32
     },
     :self => {
+        :rel => 'myself',
         :name => 'Jordan',
         :age => 28
     }
 }
 
-puts "#{my_family}"
+def family_roster(relation, information)
+    rel = relation[information][:rel]
+    name = relation[information][:name]
+    age = relation[information][:age]
+    puts "#{name} is #{rel} and is #{age}"
+end
+
+family_roster(my_family, :mother)
+family_roster(my_family, :father)
+family_roster(my_family, :sister)
+family_roster(my_family, :brother)
+family_roster(my_family, :self)
